acb9cf34162106268af11e17f5acca73e1a74fbf
peppermintmilktea <dlee047@e.ntu.edu.sg>
2023-02-17 00:09:22 +0800

    add

--- algorithm/server.py
+++ algorithm/server.py
@@ -15,7 +15,7 @@ try:
     while True:
         # Read the length of the image as a 32-bit unsigned int. If the
         # length is zero, quit the loop
-        image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
+        #image_len = struct.unpack('<L', connection.read(struct.calcsize('<L')))[0]
         if not image_len:
             break
         # Construct a stream to hold the image data and read the image
