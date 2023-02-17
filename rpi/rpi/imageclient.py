913a54cfc652d5c660728946beddc7df4c94b097
peppermintmilktea <dlee047@e.ntu.edu.sg>
2023-02-17 09:17:32 +0800

    Create imageclient.py

---
+++ rpi/rpi/imageclient.py
@@ -0,0 +1,42 @@
+# import the necessary packages
+from imutils.video import VideoStream
+from picamera import PiCamera
+from picamera.array import PiRGBArray
+import imagezmq
+import argparse
+import socket
+import time
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-s", "--server-ip", required=True,
+	help="ip address of the server to which the client will connect")
+args = vars(ap.parse_args())
+# initialize the ImageSender object with the socket address of the
+# server
+sender = imagezmq.ImageSender(connect_to="tcp://{}:5555".format(
+	args["server_ip"]))
+
+
+# get the host name, initialize the video stream, and allow the
+# camera sensor to warmup
+rpiName = socket.gethostname()
+
+# #connect to PiCamera and take capture from there
+# vs = VideoStream(usePiCamera=True, resolution=(320, 240)).start() #choose resolution 
+# #vs = VideoStream(src=0).start()
+# time.sleep(2.0)
+ 
+# while True:
+# 	# read the frame from the camera and send it to the server
+# 	frame = vs.read()
+# 	sender.send_image(rpiName, frame)
+while True: 
+	option = input("Take picture")
+	if option == "y":
+		camera = PiCamera(resolution=(640, 640))
+		rawCapture = PiRGBArray(camera)
+		camera.capture(rawCapture, format="bgr")
+		image = rawCapture.array
+		rawCapture.truncate(0)
+		sender.send_image(rpiName, image)
+							
