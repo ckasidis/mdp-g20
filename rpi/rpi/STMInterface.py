5b48f9665653a8b490fa7fea9df3ecbda6cb18d6
peppermintmilktea <dlee047@e.ntu.edu.sg>
2023-02-28 19:58:36 +0800

    Update STMInterface.py

--- rpi/rpi/STMInterface.py
+++ rpi/rpi/STMInterface.py
@@ -88,11 +88,11 @@ class STM:
             print("move() while loop")
             #bytesToRead = self.STM_connection.inWaiting()
             raw_dat = self.STM_connection.read(1)
+            print("raw_dat: " + str(raw_dat))
             dat = raw_dat.strip().decode()
+            print("dat: " + dat)
             # test_dat = raw_dat.decode().strip()
             # test_decode = raw_dat.decode()
-            print("dat: " + dat)
-            print("raw_dat: " + str(raw_dat))
             # print("test_dat: " + test_dat)
             # print("test_decode: " + test_decode)
             if dat == 'R':
