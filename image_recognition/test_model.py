7c555dc90a1f61dbb2f06fb1909d228233ed77f3
Siddhant Pathak <siddhant005@e.ntu.edu.sg>
2023-03-03 06:44:55 +0800

    ir changes

--- image_recognition/test_model.py
+++ image_recognition/test_model.py
@@ -6,15 +6,12 @@ import imutils
 import cv2
 import torch
 
-
-
 model_path = 'C:/Users/siddh/Desktop/mdp-g20/image_recognition/yolov5/content/yolov5' # local
 wts_path = 'C:/Users/siddh/Desktop/mdp-g20/image_recognition/best-model.pt'
-model = torch.hub.load(model_path, 'custom', path=wts_path, source='local')  # local repo
+model = torch.hub.load(model_path, 'custom', path=wts_path, source='local', verbose=False)  # local repo
 print("Model loaded")
 
-
-img = cv2.imread('image1.jpg')
+img = cv2.imread('image2.jpg')
 resized = cv2.resize(img,(640,640))
 
 resized = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
